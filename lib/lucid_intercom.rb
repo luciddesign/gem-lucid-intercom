1b0637c4fca9a09eddf92e255edb359eccbe7b67
Kelsey Judson <kelseyjudson@protonmail.com>
2018-01-22 18:34:51 +1300

    Alphabetical order

--- lib/lucid_intercom.rb
+++ lib/lucid_intercom.rb
@@ -1,7 +1,7 @@
 # Primarily for Bundler.
 
-require_relative './lucid_intercom/credentials'
 require_relative './lucid_intercom/attributes'
+require_relative './lucid_intercom/credentials'
 require_relative './lucid_intercom/events'
 require_relative './lucid_intercom/render_snippet'
 require_relative './lucid_intercom/send_event'
