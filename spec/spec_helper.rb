460169c525fdd0eae456190c5f71e9ffc31a1608
Kelsey Judson <kelseyjudson@protonmail.com>
2018-03-12 15:32:23 +1300

    Fake configuration for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 
 $LOAD_PATH << __dir__ + '/../lib'
+
+require 'lucid_intercom/config'
+
+LucidIntercom.config = LucidIntercom::Config.new(
+  'fake_access_token',
+  'fake_secret',
+  'fake_app_id',
+  'fake_app_prefix'
+)
