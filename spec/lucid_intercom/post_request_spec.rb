cc25b9be1969a49f0720c8465f75bd4f15c67801
Kelsey Judson <kelseyjudson@protonmail.com>
2018-03-22 03:10:36 +1300

    Spec post_request

---
+++ spec/lucid_intercom/post_request_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'lucid_intercom/post_request'
+
+RSpec.describe LucidIntercom::PostRequest do
+  let(:post_request) { LucidIntercom::PostRequest.new }
+
+  it 'returns a response' do
+    http = double
+    http_response = double(:code => 200)
+    allow(http).to receive_message_chain(:headers, :post => http_response)
+    stub_const('HTTP', http)
+
+    res = post_request.('fake/path', fake: 'param')
+
+    expect(res.status_code).to be(200)
+  end
+end
