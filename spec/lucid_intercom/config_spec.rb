c7d4d67fb2edf5eaaa7fe63bbcfcd941bb2d822f
Kelsey Judson <kelseyjudson@protonmail.com>
2018-03-26 18:57:34 +1300

    Refactor config spec

--- spec/lucid_intercom/config_spec.rb
+++ spec/lucid_intercom/config_spec.rb
@@ -2,28 +2,25 @@
 
 require 'lucid_intercom/config'
 
-RSpec.describe LucidIntercom::Config do
-  let(:config) { LucidIntercom.config }
-
-  it 'has attributes' do
-    expect(config.access_token).to eq('fake')
-    expect(config.secret).to eq('fake')
-    expect(config.app_id).to eq('fake')
-    expect(config.app_prefix).to eq('fake')
-  end
-
-  it 'has attributes forwarded from the root module' do
-    expect(LucidIntercom.access_token).to eq('fake')
-    expect(LucidIntercom.secret).to eq('fake')
-    expect(LucidIntercom.app_id).to eq('fake')
-    expect(LucidIntercom.app_prefix).to eq('fake')
-  end
+RSpec.describe LucidIntercom.config do
+  it { is_expected.to have_attributes(access_token: 'fake') }
+  it { is_expected.to have_attributes(secret: 'fake') }
+  it { is_expected.to have_attributes(app_id: 'fake') }
+  it { is_expected.to have_attributes(app_prefix: 'fake') }
 
   it 'raises an error when unset' do
-    old_config = config
-
     LucidIntercom.config = nil
-    expect { LucidIntercom.config.app_prefix }.to raise_error(LucidIntercom::NotConfiguredError)
-    LucidIntercom.config = old_config
+
+    expect { LucidIntercom.config.api_key }.to raise_error(LucidIntercom::NotConfiguredError
+)
+
+    LucidIntercom.config = subject
   end
+end
+
+RSpec.describe LucidIntercom do
+  it { is_expected.to have_attributes(access_token: 'fake') }
+  it { is_expected.to have_attributes(secret: 'fake') }
+  it { is_expected.to have_attributes(app_id: 'fake') }
+  it { is_expected.to have_attributes(app_prefix: 'fake') }
 end
